5c64d9340fa1a7f4dadeb7c663f5b0e52f64b228
Charalampos Koundourakis <CharKoundourakis@gmail.com>
2017-02-21 12:36:28 +0000

    Camera and preetiness

---
+++ Assets/CameraController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    private Vector3 OrigPos;
+    public Transform player;
+	// Use this for initialization
+	void Start ()
+	{
+	    OrigPos = transform.position;
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    transform.position = player.position + OrigPos;
+
+	}
+}
