8a229334007fabc7c0c199ab997e1db522df6a43
MarcUbach <marc.ubach.martinez@gmail.com>
2017-02-23 11:01:38 +0000

    Assets for everything
    Assets for all changes made

---
+++ Assets/Assets/Scripts/CloudScrolling.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudScrolling : MonoBehaviour
+{
+
+    public float scrollingSpeed;
+    public float loopArroundPosition;
+    public float endPosition;
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        transform.Translate(Vector3.right * scrollingSpeed);
+
+        if (transform.position.x >= endPosition)
+        {
+            Vector3 position = transform.position;
+            position.x = loopArroundPosition;
+            transform.position = position;
+
+        }
+    }
+}
---
+++ Assets/Assets/Scripts/FallingPlatform.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FallingPlatform : MonoBehaviour
+{
+
+    public float delayTime;
+
+    private bool steppedOn;
+
+    private Rigidbody rigidBody;
+
+    private Vector3 originalPosition;
+
+    private float timer;
+
+    void Start()
+    {
+        timer = delayTime;
+        steppedOn = false;
+        originalPosition = transform.position;
+
+        rigidBody = GetComponent<Rigidbody>();
+        rigidBody.isKinematic = true;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            steppedOn = true;
+        }
+    }
+
+    void Update()
+    {
+        if (steppedOn)
+        {
+            timer -= Time.deltaTime;
+            if (timer <= 0f)
+            {
+                rigidBody.useGravity = true;
+                rigidBody.isKinematic = false;
+
+            }
+        }
+    }
+
+    public void Respawn()
+    {
+        transform.position = originalPosition;
+        steppedOn = false;
+        rigidBody.velocity = Vector3.zero;
+        rigidBody.useGravity = false;
+        rigidBody.isKinematic = true;
+        timer = delayTime;
+    }
+
+}
